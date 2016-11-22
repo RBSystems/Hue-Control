794c9539af54e93b59902db814337d17166eeddd
Charlie Langridge <charlie@charlielangridge.com>
2016-11-22 06:59:35 +0000

    Bridge Finder Basic

---
+++ Simpl Sharp/HueControlBridgeFInder/HueControlBridgeFInder/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("HueControlBridgeFInder")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HueControlBridgeFInder")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
